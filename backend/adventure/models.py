ee513a991226c0ee0bf7a9ec74eff04b84cea6e5
anzani-hun <anzani@hun>
2024-04-29 17:43:39 +0200

    teszt

--- backend/adventure/models.py
+++ backend/adventure/models.py
@@ -5,4 +5,6 @@ class Location(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     targets = models.ManyToManyField(Location, on_delete=models.SET_NULL, blank=True, null=True)
-    
+
+    def __str__(self):
+        return self.name
